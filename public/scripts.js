ee2273faca3879fffdd47b8a2645f88ca02866f6
Ying James <yabjames0617@outlook.com>
2020-12-31 09:01:19 -0500

    change fetch POST data to json instead of text

--- public/scripts.js
+++ public/scripts.js
@@ -3,10 +3,13 @@ function buttonClicked() {
   clickButton.addEventListener("click", () => {
     const query = document.getElementById("search").value;
     console.log("query:", query);
+    const data = { query };
     const options = {
       method: "POST",
-      headers: { "Content-Type": "text/plain" },
-      body: query,
+      headers: { 
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
     };
     // send chinese search to ibm-translate api in index.js
     const response = fetch("/api", options);
