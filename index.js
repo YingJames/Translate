f006811ee7585d602b2b77b35e9942a93063683f
Ying James <yabjames0617@outlook.com>
2020-12-31 08:19:57 -0500

    add function for creating gifs in the dom

--- index.js
+++ index.js
@@ -41,7 +41,28 @@ function Translate(chineseInput) {
     .then(english => {
       let translationResult = document.getElementById("translationResult");
       translationResult.innerHTML = `${translateParams.text} => ${english}`;
-      setup(english);
+      addGIF(english);
     })
     .catch(err => console.log("error:", err));
+}
+
+function addGIF(query) {
+  const apiKey = "i0YNx60dA7EtWa0WIt3490EklKT1h7Ib";
+  const limit = "1";
+  const offset = "0";
+  const rating = "g";
+  const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=${limit}&offset=${offset}&rating=${rating}&lang=en`;
+  getGIF(giphyAPI)
+    .then(url => {
+      const gif = document.getElementById("giphy");
+      gif.src = url;
+    })
+    .catch(err => console.error("You have an error"));
+}
+
+async function getGIF(giphyAPI) {
+  const response = await fetch(giphyAPI);
+  const json = await response.json();
+  const url = json.data[0].images["original"].url;
+  return url;
 }
