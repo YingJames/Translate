a72f3b71613bc3dd7660c5aa81aa8a59f1eeffae
Ying James <yabjames0617@outlook.com>
2020-12-31 08:13:19 -0500

    fix formatting using Prettier

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const express = require("express");
 const app = express();
 
 // IF anybody wants to connect, you need to start listening
-app.listen(3000, () => console.log('listening on Port 3000'));
-app.use(express.static('public'));
+app.listen(3000, () => console.log("listening on Port 3000"));
+app.use(express.static("public"));
 
 // ~~~~~~~~~~~~~
 
-// Receive chinese search 
-app.post('/api', (request, response) => {
+// Receive chinese search
+app.post("/api", (request, response) => {
   // console.log('request on index.js:', request);
   const query = request;
   Translate(query);
@@ -19,26 +19,30 @@ app.post('/api', (request, response) => {
 
 function Translate(chineseInput) {
   const languageTranslator = new LanguageTranslatorV3({
-    version: '2018-05-01',
+    version: "2018-05-01",
     authenticator: new IamAuthenticator({
-      apikey: '0X3TZ4DS7Ck7osm8Yh7FnCeRpFZ6wAhqisW8zuStG4bW',
+      apikey: "0X3TZ4DS7Ck7osm8Yh7FnCeRpFZ6wAhqisW8zuStG4bW",
     }),
-    serviceUrl: 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/5fa8162f-5fa2-4751-9143-d525052ae610',
+    serviceUrl:
+      "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/5fa8162f-5fa2-4751-9143-d525052ae610",
   });
-  
+
   const translateParams = {
     text: chineseInput,
-    modelId: 'zh-en',
+    modelId: "zh-en",
   };
-  
-  languageTranslator.translate(translateParams)
+
+  languageTranslator
+    .translate(translateParams)
     .then(response => JSON.stringify(response, null, 2))
     .then(jsonStringify => JSON.parse(jsonStringify))
-    .then(translationResult => console.log(translationResult.result.translations[0].translation))
+    .then(translationResult =>
+      console.log(translationResult.result.translations[0].translation)
+    )
     .then(english => {
-      let translationResult = document.getElementById('translationResult')
-      translationResult.innerHTML = `${translateParams.text} => ${english}`
-      setup(english)
+      let translationResult = document.getElementById("translationResult");
+      translationResult.innerHTML = `${translateParams.text} => ${english}`;
+      setup(english);
     })
-    .catch(err => console.log('error:', err))
+    .catch(err => console.log("error:", err));
 }
